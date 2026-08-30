240d9b7cc3791a8c6834a91f823e8cbfa81d221a
asif ali <786asifakbar53@gmail.com>
2026-08-30 14:50:39 +0500

    refactor dotenv import and configuration placement in server.js

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
+import dotenv from "dotenv";
+dotenv.config({path : "./.env"});
 import dns from "dns";
 dns.setServers(["8.8.8.8","1.1.1.1"]);
 
-import dotenv from "dotenv";
+
 import { app } from "./app.js";
 import connectDB from "./config/db.js";
 
-dotenv.config({path : "./.env"});
+
 
 const serverStart = async () => {
     try{
